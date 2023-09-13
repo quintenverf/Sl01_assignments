2d59fc83eef9cdb9627ad147046f1aa2ae4ac221
quinten <qbverf@gmail.com>
2023-09-13 09:14:44 +0200

    ewaja

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
- kjfgkdgj
+ // kwam erachter dat PHP lang geleden is doordat ik lang ziek ben geweest heb ik helaas veel gemist
+
+ $name = "quinten";
+ $middelNaam = "daniel";
+ $achternaam = "verf";
+ $email = "quintenverf@gmail.com";
+ $telefoon = "061920300";
+ $hondNaam = "chicca";
+ $auto = ["mini", "smart"];
+ $thuiswonend = true;
+
+echo("$name");
+
+
 ?>
